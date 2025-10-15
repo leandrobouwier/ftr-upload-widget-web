f3cb92e9b70f37669ad3ba9029bd0a8d320f0d8a
Leandro Batista <leandrobouwierphp@gmail.com>
2025-10-14 22:22:31 -0300

    componente button ui

--- src/components/upload-widget-header.tsx
+++ src/components/upload-widget-header.tsx
@@ -1,12 +1,13 @@
 import { Minimize2 } from "lucide-react"
+import { Button } from "./ui/button"
 
 export function UploadWidgetHeader(){
     return(
         <div className="w-full p-4 py-2 bg-white/opacity border-zinc-800 border-b flex items-center justify-between">
             <span className="text-sm font-medium">Upload files </span>
-            <button>
+            <Button size="icon" className="-mr-2">
                 <Minimize2 strokeWidth={1.5} className="size-4" />
-            </button>
+            </Button>
         </div>
     )
 }
