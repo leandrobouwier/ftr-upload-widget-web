0d3dce7df382682e1ddcf97cdb236637fc885bbf
Leandro Batista <leandrobouwierphp@gmail.com>
2025-10-15 16:46:12 -0300

    header

--- src/components/upload-widget-header.tsx
+++ src/components/upload-widget-header.tsx
@@ -7,7 +7,7 @@ export function UploadWidgetHeader() {
     <div className="w-full p-4 py-2 bg-white/opacity border-zinc-800 border-b flex items-center justify-between">
       <span className="text-sm font-medium">Upload files </span>
 
-      <Collapsible.Trigger>
+      <Collapsible.Trigger asChild>
         <Button size="icon" className="-mr-2">
           <Minimize2 strokeWidth={1.5} className="size-4" />
         </Button>
