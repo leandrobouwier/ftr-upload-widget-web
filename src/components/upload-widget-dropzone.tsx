05845254a27fbc7f3bbe904ee243116b5e5f00d4
Leandro Batista <leandrobouwierphp@gmail.com>
2025-10-16 23:37:59 -0300

    upload button

--- src/components/upload-widget-dropzone.tsx
+++ src/components/upload-widget-dropzone.tsx
@@ -14,8 +14,8 @@ export function UploadWidgetDropzone() {
     return (
         <div className="px-3 flex-col gap-3">
             <div
-            data-active={isDragActive} 
-            className='cursor-pointer text-zinc-400 bg-black/20 p-5 ronded-lg border
+                data-active={isDragActive}
+                className='cursor-pointer text-zinc-400 bg-black/20 p-5 ronded-lg border
              border-zinc-700 border-dashed h-32 flex flex-col items-center justify-center gap-1
              hover:border-zinc-600 transition-colors data-[active=true]:bg-indigo-500/10 data-[active=true]:border-indigo-500
              data-[active=true]:text-indigo-400' {...getRootProps()}>
@@ -24,7 +24,9 @@ export function UploadWidgetDropzone() {
                 <span className='text-xs underline'>click to open</span>
             </div>
 
-            <span>Only PNG anda JPG files are supported</span>
+            <span className="text-xxs text-zinc-400">
+                Only PNG and JPG files are supported.
+            </span>
         </div>
     )
 }
--- src/components/upload-widget-minimized-button.tsx
+++ src/components/upload-widget-minimized-button.tsx
@@ -3,7 +3,7 @@ import { Minimize2 } from "lucide-react";
 import { UploadWidgetTitle } from "./upload-widget-title";
 export function UploadWidgetMinimizedButton() {
   return (
-    <Collapsible.Trigger className="w-full bg-white/opacity py-3 px-5 flex items-center justify-between">
+    <Collapsible.Trigger className="w-full bg-white/[var(--opacity-custom-2)] py-3 px-5 flex items-center justify-between">
       <UploadWidgetTitle />
 
       <Minimize2 strokeWidth={1.5} className="size-4 text-zinc-400 group-hover:text-zinc-100"/>
