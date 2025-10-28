e90de5ac4b734850800cd3e57f1150d6404b85c6
Leandro Batista <leandrobouwierphp@gmail.com>
2025-10-27 22:38:48 -0300

    compressed image function

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import { UploadWidget } from "./components/upload-widget";
 
 export function App() {
   return (
-   <main className="h-dvh flex flex-col items-center justify-center p-10">
+   <main className="min-h-screen flex flex-col items-center justify-center p-10 overflow-y-auto">
     <UploadWidget />
    </main>
   )
--- src/store/uploads.ts
+++ src/store/uploads.ts
@@ -4,142 +4,144 @@ import { immer } from 'zustand/middleware/immer'
 import { uploadFileToStorage } from "../http/upload-file-to-storage";
 import { CanceledError } from "axios";
 import { useShallow } from "zustand/shallow";
+import { compressImage } from "../utils/compress-image";
 
 export type Upload = {
-    name: string
-    file: File
-    abortController: AbortController
-    status: 'progress' | 'success' | 'error' | 'canceled'
-    originalSizeInbytes: number
-    uploadSizeInbytes: number
+   name: string
+   file: File
+   abortController: AbortController
+   status: 'progress' | 'success' | 'error' | 'canceled'
+   originalSizeInbytes: number
+   uploadSizeInbytes: number
 }
 
 type UploadState = {
-    uploads: Map<string, Upload>
-    addUploads: (files: File[]) => void
-    cancelUpload: (uploadId: string) => void
+   uploads: Map<string, Upload>
+   addUploads: (files: File[]) => void
+   cancelUpload: (uploadId: string) => void
 }
 
 enableMapSet()
 
 export const useUploads = create<UploadState, [['zustand/immer', never]]>(
-    immer((set, get) => {
-        async function processUpload(uploadId: string) {
-            const upload = get().uploads.get(uploadId);
+   immer((set, get) => {
 
-            if (!upload) {
-                return;
+      function updateUpload(uploadId: string, data: Partial<Upload>) {
+         const upload = get().uploads.get(uploadId);
+
+         if (!upload) {
+            return;
+         }
+
+         set(state => {
+            state.uploads.set(uploadId, { ...upload, ...data })
+         })
+      }
+
+      async function processUpload(uploadId: string) {
+         const upload = get().uploads.get(uploadId);
+
+         if (!upload) {
+            return;
+         }
+
+         try {
+
+            const compressedFile = await compressImage({
+               file: upload.file,
+               maxWidth: 200,
+               maxHeight: 200,
+               quality: 0.5,
+            })
+
+            await uploadFileToStorage(
+               {
+                  file: compressedFile,
+                  onProgress(sizeInbytes) {
+                     updateUpload(uploadId, { uploadSizeInbytes: sizeInbytes, })
+                  }
+               },
+               { signal: upload.abortController.signal }
+            )
+            updateUpload(uploadId, { status: 'success' })
+
+         } catch (err) {
+            if (err instanceof CanceledError) {
+               updateUpload(uploadId, { status: 'canceled' })
+
+               return
             }
 
-            try {
-                await uploadFileToStorage(
-                    {
-                        file: upload.file,
-                        onProgress(sizeInbytes) {
-                            set(state => {
-                                state.uploads.set(uploadId, {
-                                    ...upload,
-                                    uploadSizeInbytes: sizeInbytes,
-                                })
-                            })
-                        }
-                    },
-                    { signal: upload.abortController.signal }
-                )
+            updateUpload(uploadId, { status: 'error' })
+         }
+      }
 
-                set(state => {
-                    state.uploads.set(uploadId, {
-                        ...upload,
-                        status: 'success'
-                    })
-                })
-            } catch (err) {
-                if (err instanceof CanceledError) {
-                    set(state => {
-                        state.uploads.set(uploadId, {
-                            ...upload,
-                            status: 'canceled'
-                        })
-                    })
+      function cancelUpload(uploadId: string) {
+         const upload = get().uploads.get(uploadId);
 
-                    return
-                }
+         if (!upload) {
+            return;
+         }
 
-                set(state => {
-                    state.uploads.set(uploadId, {
-                        ...upload,
-                        status: 'error'
-                    })
-                })
+         upload.abortController.abort()
+      }
+
+      function addUploads(files: File[]) {
+         for (const file of files) {
+            const uploadId = crypto.randomUUID()
+            const abortController = new AbortController()
+
+            const upload: Upload = {
+               name: file.name,
+               file,
+               abortController,
+               status: 'progress',
+               originalSizeInbytes: file.size,
+               uploadSizeInbytes: 0,
             }
-        }
+            set(state => {
+               state.uploads.set(uploadId, upload)
+            })
 
-        function cancelUpload(uploadId: string) {
-            const upload = get().uploads.get(uploadId);
+            processUpload(uploadId);
+         }
+      }
 
-            if (!upload) {
-                return;
-            }
-
-            upload.abortController.abort()
-        }
-
-        function addUploads(files: File[]) {
-            for (const file of files) {
-                const uploadId = crypto.randomUUID()
-                const abortController = new AbortController()
-
-                const upload: Upload = {
-                    name: file.name,
-                    file,
-                    abortController,
-                    status: 'progress',
-                    originalSizeInbytes: file.size,
-                    uploadSizeInbytes: 0,
-                }
-                set(state => {
-                    state.uploads.set(uploadId, upload)
-                })
-
-                processUpload(uploadId);
-            }
-        }
-
-        return {
-            uploads: new Map(),
-            addUploads,
-            cancelUpload,
-        }
-    }))
+      return {
+         uploads: new Map(),
+         addUploads,
+         cancelUpload,
+      }
+   }))
 
 
-    export const usePendingUploads = () => {
-        return useUploads(
-          useShallow((store) => {
-            const isThereAnyPendingUploads = Array.from(store.uploads.values()).some(
-              (upload) => upload.status === "progress"
-            );
-      
-            if (!isThereAnyPendingUploads) {
-              return { isThereAnyPendingUploads, globalPercentage: 100 };
-            }
-      
-            const { total, uploaded } = Array.from(store.uploads.values()).reduce(
-              (acc, upload) => {
-                acc.total += upload.originalSizeInbytes;
-                acc.uploaded += upload.uploadSizeInbytes;
-      
-                return acc;
-              },
-              { total: 0, uploaded: 0 }
-            );
-      
-            const globalPercentage = Math.min(
-              Math.round((uploaded * 100) / total),
-              100
-            );
-      
-            return { isThereAnyPendingUploads, globalPercentage };
-          })
-        );
-      };
+export const usePendingUploads = () => {
+   return useUploads(
+      useShallow((store) => {
+         const isThereAnyPendingUploads = Array.from(store.uploads.values()).some(
+            (upload) => upload.status === "progress"
+         );
+
+         if (!isThereAnyPendingUploads) {
+            return { isThereAnyPendingUploads, globalPercentage: 100 };
+         }
+
+         const { total, uploaded } = Array.from(store.uploads.values()).reduce(
+            (acc, upload) => {
+               acc.total += upload.originalSizeInbytes;
+               acc.uploaded += upload.uploadSizeInbytes;
+
+               return acc;
+            },
+            { total: 0, uploaded: 0 }
+         );
+
+         const globalPercentage = Math.min(
+            Math.round((uploaded * 100) / total),
+            100
+         );
+
+         return { isThereAnyPendingUploads, globalPercentage };
+      })
+   );
+};
